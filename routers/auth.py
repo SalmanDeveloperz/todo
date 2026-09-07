fa6806d5db3c1487936c6e9a5d82f52051a8fb82
Muhammad Salman <chsalmanramzan422@gmail.com>
2026-09-06 23:08:19 -0700

    Added authentication, validate the users and access
    Signed-off-by: Muhammad Salman <chsalmanramzan422@gmail.com>

--- routers/auth.py
+++ routers/auth.py
@@ -5,9 +5,9 @@ from pydantic import BaseModel
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from starlette import status
-
 from database import SessionLocal
 from models import Users
+from fastapi.security import OAuth2PasswordRequestForm  # OAuth is more secure then simple fastapi
 
 router= APIRouter()
 
@@ -33,8 +33,13 @@ def get_db():
 
 db_dependency= Annotated[Session, Depends(get_db)] #depends is dependency injection really need before we execute behind scenes
 
-
-
+def authenticate_user(username:str, password:str, db):
+    user= db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hased_password):
+        return False
+    return True
 
 
 
@@ -56,3 +61,14 @@ async def create_user( db: db_dependency, create_user_request: CreateUserRequest
     )
     db.add(create_user_model)
     db.commit()
+
+
+
+@router.post("/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                                 db:db_dependency):
+    user= authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        return 'Failed authentication'
+    return 'Success authentication'
+    return form_data.username
