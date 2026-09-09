b2e2957716d67448b96893aeb6cbd3a0a54fe9fb
Muhammad Salman <chsalmanramzan422@gmail.com>
2026-09-09 03:43:54 -0700

    Format files, added test_main file, make directory clean

--- test/models.py
+++ test/models.py
@@ -1,4 +1,4 @@
-from  database import Base
+from todoApp.test.database import Base
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 
 
--- test/test_example.py
+++ test/test_example.py
@@ -1,6 +1,5 @@
 import pytest
 
-
 def test_equal_or_not_equal():
     assert 3==3
     assert 2!=3
