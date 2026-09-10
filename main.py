cfa093318ee2a554b7695fc9eee6957bb19d2a34
Muhammad Salman <chsalmanramzan422@gmail.com>
2026-09-10 14:40:49 -0800

    started full stack section

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+
 from todoApp.models import Base
 from todoApp.database import engine
 from todoApp.routers import auth, todos
+from fastapi.templating import Jinja2Templates
 
 app= FastAPI()
 
 Base.metadata.create_all(bind=engine) # this only run if todos.db not exist
+
+templates = Jinja2Templates(directory= "todoApp/templates")
+
+@app.get("/")
+def test():
+    return templates.TemplateResponse("home.html", {"request":request})
+
 
 @app.get("/healthy")
 def health_check():
