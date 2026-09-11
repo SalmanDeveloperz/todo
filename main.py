30d43ee543a02e2a57a145b6874be3c13d73fc0f
Muhammad Salman <chsalmanramzan422@gmail.com>
2026-09-10 17:30:44 -0800

    Added the frontend files, work upon the login page

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request
+from starlette.staticfiles import StaticFiles
 
 from todoApp.models import Base
 from todoApp.database import engine
@@ -11,10 +12,11 @@ Base.metadata.create_all(bind=engine) # this only run if todos.db not exist
 
 templates = Jinja2Templates(directory= "todoApp/templates")
 
-@app.get("/")
-def test():
-    return templates.TemplateResponse("home.html", {"request":request})
+app.mount("/static", StaticFiles(directory="todoApp/static"), name="static")
 
+@app.get("/")
+def test(request: Request):
+    return templates.TemplateResponse(request=request, name="login.html")
 
 @app.get("/healthy")
 def health_check():
--- routers/auth.py
+++ routers/auth.py
@@ -1,7 +1,7 @@
 from datetime import timedelta, datetime, timezone
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Request
 from pydantic import BaseModel
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
@@ -10,6 +10,7 @@ from todoApp.database import SessionLocal
 from todoApp.models import Users
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer # OAuth is more secure then simple fastapi
 from jose import jwt, JWTError
+from fastapi.templating import Jinja2Templates
 
 router= APIRouter(
     prefix='/auth',
@@ -45,6 +46,16 @@ def get_db():
 
 db_dependency= Annotated[Session, Depends(get_db)] #depends is dependency injection really need before we execute behind scenes
 
+templates= Jinja2Templates(directory="todoApp/templates")
+
+### Pages ###
+
+@router.get ("/login-page")
+def render_login_page(request: Request):
+    return templates.TemplateResponse(request=request, name="login.html")
+
+
+### Endpoints ###
 def authenticate_user(username:str, password:str, db):
     user= db.query(Users).filter(Users.username == username).first()
     if not user:
